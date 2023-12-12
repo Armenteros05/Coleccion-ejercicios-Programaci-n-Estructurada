f7afd01d9fdae731e3165b30d77a3deafb61546c
Armenteros05 <sarmerod@myuax.com>
2023-12-12 11:56:34 +0100

    Intento de arreglo

--- Estructura.cpp
+++ Estructura.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <cstdlib> // Necesario para malloc y free
+#include <cstdlib>
+#include <string>
 using namespace std;
 
 struct Estudiante {
@@ -9,48 +10,13 @@ struct Estudiante {
 };
 
 int main() {
-    // a. Instanciación normal
-    Estudiante estudiante1;
-    Estudiante estudiante2;
-    Estudiante estudiante3;
-
-    int respuesta;
-
-    cout << "Ingrese el nombre del estudiante: ";
-    cin.getline(estudiante1.nombre, 50, '\n');
-    cout << "Ingrese la edad del estudiante: ";
-    cin >> estudiante1.edad;
-    cout << "Ingrese el promedio de notas del estudiante: ";
-    cin >> estudiante1.promedio;
-    cout << "\nNombre del estudiante: " << estudiante1.nombre << endl;
-    cout << "Edad del estudiante: " << estudiante1.edad << endl;
-    cout << "Promedio de notas del estudiante: " << estudiante1.promedio << endl;
-
-    // b. Instanciación con el operador malloc
-    Estudiante* estudiante4 = static_cast<Estudiante*>(malloc(sizeof(Estudiante)));
-
-    // c. Punteros y estructuras
-    Estudiante* estudiante5 = new Estudiante;
-
-    // d. Inicialización del puntero
-    cout << "Ingrese el nombre del estudiante 2: ";
-    cin.ignore(); // Ignora el salto de línea pendiente
-    cin.getline(estudiante5->nombre, 50, '\n');
-    cout << "Ingrese la edad del estudiante 2: ";
-    cin >> estudiante5->edad;
-    cout << "Ingrese el promedio de notas del estudiante 2: ";
-    cin >> estudiante5->promedio;
-    cout << "\nNombre del estudiante 5: " << estudiante5->nombre << endl;
-    cout << "Edad del estudiante 5: " << estudiante5->edad << endl;
-    cout << "Promedio de notas del estudiante 5: " << estudiante5->promedio << endl;
-
-    // e. Aplicación que crea, añade, muestra y elimina estudiantes
     const int MAX_ESTUDIANTES = 5;
     Estudiante* listaEstudiantes[MAX_ESTUDIANTES];
     int cantidadEstudiantes = 0;
+    int respuesta;
 
     while (true) {
-        cout << "\nSeleccione una opción:\n1. Agregar estudiante\n2. Mostrar estudiantes\n3. Salir\n";
+        cout << "\nSeleccione una opcion:\n1. Agregar estudiante\n2. Mostrar estudiantes\n3. Buscar estudiante\n4. Salir\n";
         cin >> respuesta;
 
         switch (respuesta) {
@@ -58,7 +24,7 @@ int main() {
                 if (cantidadEstudiantes < MAX_ESTUDIANTES) {
                     listaEstudiantes[cantidadEstudiantes] = new Estudiante;
                     cout << "Ingrese el nombre del estudiante: ";
-                    cin.ignore(); // Ignora el salto de línea pendiente
+                    cin.ignore();
                     cin.getline(listaEstudiantes[cantidadEstudiantes]->nombre, 50, '\n');
                     cout << "Ingrese la edad del estudiante: ";
                     cin >> listaEstudiantes[cantidadEstudiantes]->edad;
@@ -77,17 +43,37 @@ int main() {
                 }
                 break;
             case 3:
+                // Buscar estudiante por nombre
+            {
+                string nombreBuscar;
+                cout << "Ingrese el nombre del estudiante a buscar: ";
+                cin.ignore();
+                getline(cin, nombreBuscar);
+
+                bool encontrado = false;
+                for (int i = 0; i < cantidadEstudiantes; ++i) {
+                    if (nombreBuscar == listaEstudiantes[i]->nombre) {
+                        cout << "Estudiante encontrado:\n";
+                        cout << "Nombre: " << listaEstudiantes[i]->nombre << ", Edad: " << listaEstudiantes[i]->edad
+                             << ", Promedio: " << listaEstudiantes[i]->promedio << endl;
+                        encontrado = true;
+                        break;
+                    }
+                }
+
+                if (!encontrado) {
+                    cout << "Estudiante no encontrado." << endl;
+                }
+            }
+                break;
+            case 4:
                 // Liberar memoria antes de salir
                 for (int i = 0; i < cantidadEstudiantes; ++i) {
                     delete listaEstudiantes[i];
                 }
-                // Liberar memoria para el estudiante4
-                free(estudiante4);
-                // Liberar memoria para el estudiante5
-                delete estudiante5;
                 return 0;
             default:
-                cout << "Opcion no válida. Intentelo de nuevo." << endl;
+                cout << "Opción no válida. Inténtelo de nuevo." << endl;
         }
     }
 
