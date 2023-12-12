1041f0e51b3ec3113bad35516e893a3d0adcd0f9
Armenteros05 <sarmerod@myuax.com>
2023-12-12 12:26:24 +0100

    Ejercicio 5 finalizado

--- Estructuras y funciones.cpp
+++ Estructuras y funciones.cpp
@@ -1,56 +1,64 @@
 #include <iostream>
 #include <cstring>
 
+// Estructura Estudiante
 struct Estudiante {
     char nombre[50];
     int edad;
     float promedio;
 };
 
+// a. Pasar una estructura por valor como argumento
 void imprimirEstudiantePorValor(Estudiante est) {
     std::cout << "a. Por valor: Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << std::endl;
 }
 
+// b. Pasar una estructura por referencia como argumento
 void modificarEstudiantePorReferencia(Estudiante& est) {
-    std::cout << "Introduce la nueva edad: ";
-    std::cin >> est.edad;
+    est.edad = 30;
 }
 
+// c. Pasar una estructura por dirección como argumento
 void imprimirEstudiantePorDireccion(const Estudiante* est) {
-    std::cout << "c. Por direccion: Nombre: " << est->nombre << ", Edad: " << est->edad << ", Promedio: " << est->promedio << std::endl;
+    std::cout << "c. Por dirección: Nombre: " << est->nombre << ", Edad: " << est->edad << ", Promedio: " << est->promedio << std::endl;
 }
 
+// d. Programación orientada a objetos
 class EstudianteOOP {
 public:
     char nombre[50];
     int edad;
     float promedio;
 
+    // Constructor
     EstudianteOOP(const char* n, int e, float p) : edad(e), promedio(p) {
         std::strcpy(nombre, n);
     }
 
+    // Método para imprimir información del estudiante
     void imprimirInformacion() const {
-        std::cout << "d. Programacion orientada a objetos: Nombre: " << nombre << ", Edad: " << edad << ", Promedio: " << promedio << std::endl;
+        std::cout << "d. Programación orientada a objetos: Nombre: " << nombre << ", Edad: " << edad << ", Promedio: " << promedio << std::endl;
     }
 };
 
 int main() {
+    // Crear una instancia de la estructura Estudiante
     Estudiante estudiante1;
-    std::cout << "Introduce el nombre del estudiante: ";
-    std::cin.ignore(); // Ignorar el salto de línea anterior
-    std::cin.getline(estudiante1.nombre, 50);
+    std::strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 25;
+    estudiante1.promedio = 8.5;
 
-    std::cout << "Introduce la edad del estudiante: ";
-    std::cin >> estudiante1.edad;
-
-    std::cout << "Introduce el promedio del estudiante: ";
-    std::cin >> estudiante1.promedio;
-
+    // a. Pasar una estructura por valor como argumento
     imprimirEstudiantePorValor(estudiante1);
+
+    // b. Pasar una estructura por referencia como argumento
     modificarEstudiantePorReferencia(estudiante1);
+    std::cout << "b. Por referencia: Edad después de modificar: " << estudiante1.edad << std::endl;
+
+    // c. Pasar una estructura por dirección como argumento
     imprimirEstudiantePorDireccion(&estudiante1);
 
+    // d. Programación orientada a objetos
     EstudianteOOP estudianteOOP("Maria", 22, 9.0);
     estudianteOOP.imprimirInformacion();
 
